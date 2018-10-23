f7fab4f4853f66baf4be0685e24f596762d77250
MinkyungOh <ominomin73@gmail.com>
2018-10-23 13:38:01 +0900

    Initial commit

---
+++ app/src/main/java/com/omin/www/myapp1022_1/MainActivity.java
@@ -0,0 +1,21 @@
+package com.omin.www.myapp1022_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onclickwidget(View view) {
+        String message = ((Button)view).getText().toString() + " 클릭했어요.";
+        Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
+    }
+}
